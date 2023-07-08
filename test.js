a854255e62ff5d3887ae63dbc43cd0b58b0f3d45
murshud <murshud@gmail.com>
2023-07-08 11:15:00 +0400

    salam

---
+++ test.js
@@ -0,0 +1,4 @@
+let a = +prompt('duzbucaglinin a terfeni daxil edin')
+let b = +prompt('duzbucaglinin b terefini daxil edin')
+let sh = a * b ;
+document.getElementById('murik').innerHTML = ' A terefi ' + a + ' sm olan duzbucaglinin, B terefi ' + b + ' sm olarsa , bu duzbucalinin sahesi  ' + sh + ' sm olar. '
